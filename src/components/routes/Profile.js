ca456d66435686cdb672fd217b280e6625b698a9
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-04-26 00:37:09 +0100

    Implement my profile functionalities

--- src/components/routes/Profile.js
+++ src/components/routes/Profile.js
@@ -1,13 +1,37 @@
-/** ****************************************
- * Project: react-redux-group-project
- * File: Profile.js
- * Created: 4/24/23
- * Author: Abdullah Al Mamun <mamun1214@gmail.com>
- ****************************************** */
 import React from 'react';
+import { useSelector } from 'react-redux';
+import './Profile.css';
 
-const Profile = () => (
-  <div />
-);
+function Profile() {
+  const { missions } = useSelector((store) => store.missions);
+  const rockets = useSelector((store) => store.rockets);
+  const filteredMissions = missions.filter((mission) => mission.reserved === true);
+  const filteredRockets = rockets.filter((rocket) => rocket.reserved === true);
+  return (
+    <main>
+      <div className="main-container">
+        <div className="missions">
+          <h3 className="rocket-main">my rockets</h3>
+          <ul>
+            { filteredRockets.map((rocket) => {
+              const rocketId = rocket.id;
+              return <li key={rocketId}>{rocket.name}</li>;
+            }) }
+          </ul>
+        </div>
+        <div className="missions">
+          <h3 className="mission-main">my missions</h3>
+          <ul>
+            { filteredMissions.map((mission) => {
+              const id = mission.mission_id;
+              const name = mission.mission_name;
+              return <li key={id}>{name}</li>;
+            }) }
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
+}
 
 export default Profile;
