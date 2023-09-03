191f6b7cb2b03085706cb55db64fe1d6cd6a8a76
Taiwo Enoch <111912283+TaiwoEnoch@users.noreply.github.com>
2023-09-03 12:59:32 +0100

    Update missionsSlice.js

--- src/components/redux/features/missions/missionsSlice.js
+++ src/components/redux/features/missions/missionsSlice.js
@@ -1,9 +1,3 @@
-/** ****************************************
- * Project: react-redux-group-project
- * File: missionsSlice.js
- * Created: 4/24/23
- * Author: Abdullah Al Mamun <mamun1214@gmail.com>
- ****************************************** */
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
